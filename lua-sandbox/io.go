e9374d2cecb042f5285db89bf82f41713a4bcad8
Gaetan de Villele <gdevillele@gmail.com>
2017-02-28 15:36:09 -0800

    handle lua defined commands
    Signed-off-by: Gaetan de Villele <gdevillele@gmail.com>

---
+++ lua-sandbox/io.go
@@ -0,0 +1,33 @@
+package sandbox
+
+import (
+	"fmt"
+
+	lua "github.com/yuin/gopher-lua"
+)
+
+// print takes one string argument and write its content into the cli out stream.
+func (s *Sandbox) print(L *lua.LState) int {
+
+	argc := L.GetTop() // get number of arguments
+	if argc <= 0 {
+		return 0 // do nothing and return
+	}
+
+	args := make([]lua.LValue, argc)
+	for i := -argc; i < 0; i++ {
+		args[i+argc] = L.Get(i)
+	}
+	L.Pop(argc)
+
+	// fmt.Println(args)
+	for i, arg := range args {
+		fmt.Printf("%s", arg.String())
+		if i < len(args)-1 { // for all but last element
+			fmt.Printf(" ")
+		} else {
+			fmt.Printf("\n")
+		}
+	}
+	return 0
+}
