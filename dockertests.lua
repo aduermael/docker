44d35e609eb11be1e5ab30e3504886714428bef6
Adrien Duermael <adrien@duermael.com>
2017-03-29 09:43:08 -0700

    test for docker.container.inspect
    Signed-off-by: Adrien Duermael <adrien@duermael.com>

--- dockertests.lua
+++ dockertests.lua
@@ -6,8 +6,28 @@ Tests = { id = "1" }
 
 function Tests:run()
   print("RUNNING tests...")
-  print(self.id)
+  self:testDockerContainerInspect()
 end
+
+function Tests:testDockerContainerInspect()
+	local testName = "docker.container.inspect"
+	print('TEST: ' .. testName)
+
+	-- cleanup to avoid collisions
+	pcall(docker.silentCmd, 'rm -fv ' .. testName)
+	
+	local containerID = docker.silentCmd('run -ti -d --name ' .. testName .. ' alpine:3.5 ash')
+	local container = docker.container.inspect(containerID)[1]
+	if container.name ~= testName then
+		error("container name is not the one expected")
+	end
+
+	-- cleanup
+	docker.silentCmd('rm -fv ' .. testName)
+
+	print("SUCCESS")
+end
+
 
 local tests = Tests
 tests.id = "2"
