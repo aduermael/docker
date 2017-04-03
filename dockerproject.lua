70dbb01745486c2bff0ac1eb602d47864431e911
Adrien Duermael <adrien@duermael.com>
2017-04-03 09:42:09 -0700

    dockerscripts
    Signed-off-by: Adrien Duermael <adrien@duermael.com>

---
+++ dockerproject.lua
@@ -0,0 +1,183 @@
+-- Docker project configuration
+
+tests = require("dockertests")
+
+project = {
+    id = "62b97e4d-f0e2-4459-9485-93554831db31",
+    name = "docker",
+    root = project.root,
+}
+
+project.tasks = {
+    -- using anonymous functions when target function is not yet defined
+    binaries = {function() binaries() end, 'export docker cli binaries'},
+    compose = {function(args) compose(args) end, 'equivalent to docker-compose'},
+    dev = {function(args) dev(args) end, 'develop in container'},
+    status = {function() status() end, 'shows project status'},
+    test = {function() tests:run() end, 'runs Lua tests'},
+}
+
+-- Behaves like docker-compose binary (https://docs.docker.com/compose/)
+function compose(args)
+    local jsonstr, err = docker.silentCmd('run --rm -e HOST_BIND_MOUNTS=1 ' ..
+        '-w ' .. project.root .. ' ' ..
+        'aduermael/compose ' .. utils.join(args, ' '))
+    if err ~= nil then
+        error(err)
+    end
+
+    local bindMounts = json.decode(jsonstr)
+    for i,bindmount in ipairs(bindMounts) do
+        bindMounts[i] = '-v ' .. bindmount .. ':'  .. bindmount .. ':ro'
+    end
+
+    pcall(docker.cmd, 'run --rm ' ..
+        '-w ' .. project.root .. ' ' ..
+        '-v /var/run/docker.sock:/var/run/docker.sock ' ..
+        utils.join(bindMounts, ' ') .. ' ' ..
+        'aduermael/compose -p ' .. project.name .. ' ' ..
+        utils.join(args, ' '))
+end
+
+-- Creates a Docker image with full dev environment
+function build()
+    print("Assembling full dev environment... (this is slow the first time)")
+    docker.cmd('build -t docker .')
+end
+
+-- Mounts your source in an interactive container
+function dev(args)
+    build()
+    local argsStr = utils.join(args, " ")
+    docker.cmd('run \
+    ' .. argsStr .. ' \
+    -v ' .. os.home() .. '/.docker/.testuserid:/root/.docker/.testuserid \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v ' .. project.root .. ':/go/src/github.com/docker/docker \
+    --privileged \
+    -i \
+    -t \
+    docker bash')
+end
+
+function binaries()
+    local exportDir = project.root .. '/docker-init/binaries'
+    build()
+    local command = 'run ' ..
+        '-e DOCKER_CROSSPLATFORMS="linux/amd64 linux/arm darwin/amd64 windows/amd64" ' ..
+        '-v ' .. exportDir .. ':/output ' ..
+        '-v ' .. project.root .. ':/go/src/github.com/docker/docker ' ..
+        '--privileged -i -t docker bash -c "' ..
+        'VERSION=$(< ./VERSION) && ' ..
+        'hack/make.sh cross-client && ' ..
+        'mkdir -p /output/linux && ' ..
+        'mkdir -p /output/linux-arm && ' ..
+        'mkdir -p /output/mac && ' ..
+        'mkdir -p /output/windows && ' ..
+        'pushd bundles/$VERSION/cross-client/linux/amd64 && mv docker-$VERSION docker && zip /output/linux/docker.zip docker && popd && ' ..
+        'pushd bundles/$VERSION/cross-client/linux/arm && mv docker-$VERSION docker && zip /output/linux-arm/docker.zip docker && popd && ' ..
+        'pushd bundles/$VERSION/cross-client/darwin/amd64 && mv docker-$VERSION docker && zip /output/mac/docker.zip docker && popd && ' ..
+        'pushd bundles/$VERSION/cross-client/windows/amd64 && mv docker-$VERSION.exe docker.exe && zip /output/windows/docker.zip docker.exe"'
+    docker.cmd(command)
+end
+
+-- Lists Docker entities involved in project
+function status()
+    local dockerhost = os.getEnv("DOCKER_HOST")
+    if dockerhost == "" then
+        dockerhost = "local"
+    end
+    print("Docker host: " .. dockerhost)
+
+    local swarmMode, err = isSwarmMode()
+    if err ~= nil then
+        print('error:', err)
+        return
+    end
+
+    if swarmMode then
+        local services = docker.service.list('--filter label=docker.project.id:' .. project.id)
+        print("Services:")
+        if #services == 0 then
+            print("none")
+        else
+            for i, service in ipairs(services) do
+                print(" - " .. service.name .. " image: " .. service.image)
+            end
+        end
+    else
+        local containers = docker.container.list('-a --filter label=docker.project.id:' .. project.id)
+        print("Containers:")
+        if #containers == 0 then
+            print("none")
+        else
+            for i, container in ipairs(containers) do
+                print(" - " .. container.name .. " (" .. container.status .. ") image: " .. container.image)
+            end
+        end
+    end
+
+    local volumes = docker.volume.list('--filter label=docker.project.id:' .. project.id)
+    print("Volumes:")
+    if #volumes == 0 then
+        print("none")
+    else
+        for i, volume in ipairs(volumes) do
+            print(" - " .. volume.name .. " (" .. volume.driver .. ")")
+        end
+    end
+
+    local networks = docker.network.list('--filter label=docker.project.id:' .. project.id)
+    print("Networks:")
+    if #networks == 0 then
+        print("none")
+    else
+        for i, network in ipairs(networks) do
+            print(" - " .. network.name .. " (" .. network.driver .. ")")
+        end
+    end
+
+    local images = docker.network.list('--filter label=docker.project.id:' .. project.id)
+    print("Images (built within project):")
+    if #networks == 0 then
+        print("none")
+    else
+        for i, image in ipairs(images) do
+            print(" - " .. image.name)
+        end
+    end
+end
+
+-- indicates whether the targeted daemon runs in swarm mode
+function isSwarmMode() -- bool, err
+    local out, err = docker.silentCmd("info --format '{{ .Swarm.LocalNodeState }}'")
+    if err ~= nil then
+        return false, err
+    end
+    return out == 'active', nil
+end
+
+----------------
+-- UTILS
+----------------
+
+utils = {}
+
+-- returns a string combining strings from  string array in parameter
+-- an optional string separator can be provided.
+utils.join = function(arr, sep)
+    str = ""
+    if sep == nil then
+        sep = ""
+    end
+    if arr ~= nil then
+        for i,v in ipairs(arr) do
+            if str == "" then
+                str = v
+            else
+                str = str .. sep ..  v
+            end
+        end
+    end
+    return str
+end
---
+++ dockertests.lua
@@ -0,0 +1,208 @@
+--
+-- images test
+--
+
+Tests = { currentTest = "", eraser = "" }
+
+function Tests:cmd(str) 
+	local success, out = pcall(docker.silentCmd, str)
+	if success == false then
+		-- out is an error in that case
+		self:fail(out)
+	end
+	return out
+end
+
+function Tests:start(testName)
+	self.currentTest = testName
+	-- used to erase lines...
+	-- +3 because of prefix emoji
+	self.eraser = ""
+	for i=1,string.len(self.currentTest) + 3 do
+		self.eraser = self.eraser .. " "
+	end
+	printf('⚙️  %s', self.currentTest)
+end
+
+function Tests:fail(message)
+	printf('\r%s', self.eraser)
+	printf('\r❌  %s\n', self.currentTest)
+	error(message)
+end
+
+function Tests:log(...)
+	printf('\r%s\r', self.eraser)
+	print(...)
+	printf('⚙️  %s', self.currentTest)
+end
+
+function Tests:success()
+	printf('\r%s', self.eraser)
+	printf('\r👍  %s\n', self.currentTest)
+end
+
+function Tests:run()
+	self:testDockerImageInspect()
+	self:testDockerContainerInspect()
+	self:testProjectScope()
+	self:testVolumeCreate()
+	self:testNetworkCreate()
+end
+
+function Tests:testDockerImageInspect()
+	self:start("docker.image.inspect()")
+
+	self:cmd('pull alpine:3.5')
+	self:cmd('image tag alpine:3.5 myalpine')
+
+	local image = docker.image.inspect('myalpine')[1]
+
+	self:log("id: " .. image.id)
+	self:log("repo tags: " .. unpack(image.repoTags))
+
+	self:cmd('image rm myalpine')
+
+	self:success()
+end
+  
+function Tests:testDockerContainerInspect()
+	self:start("docker.container.inspect()")
+
+	-- cleanup to avoid collisions
+	pcall(docker.silentCmd,'rm -fv docker.container.inspect')
+	
+	local containerID = self:cmd('run -ti -d --name docker.container.inspect alpine:3.5 ash')
+	local container = docker.container.inspect(containerID)[1]
+	if container.name ~= 'docker.container.inspect' then
+		self:fail("container name is not the one expected")
+	end
+
+	-- cleanup
+	self:cmd('rm -fv docker.container.inspect')
+
+	self:success()
+end
+
+function Tests:testProjectScope()
+	self:start("project scope")
+
+	local scope1 = "com.docker.test.scope.id.1"
+	local scope2 = "com.docker.test.scope.id.2"
+	local containers = nil
+
+	self:log("cleanup for both test scopes")
+
+	-- clean both test scopes
+	project.id = scope1
+	containers = docker.container.list()
+	for i,container in ipairs(containers) do
+		self:cmd('rm -f ' .. container.id)
+	end
+	project.id = scope2
+	containers = docker.container.list()
+	for i,container in ipairs(containers) do
+		self:cmd('rm -f ' .. container.id)
+	end
+
+	-- make sure both scopes are clean
+	project.id = scope1
+	containers = docker.container.list()
+	if #containers ~= 0 then
+		self:fail('scope #1 not clean')
+	end
+	project.id = scope2
+	containers = docker.container.list()
+	if #containers ~= 0 then
+		self:fail('scope #2 not clean')
+	end
+
+	self:log("remove container with generated name")
+
+	-- run container with no name
+	-- try to remove it using its name
+	self:cmd('run -ti -d --label generated_name alpine:3.5 ash')
+	containers = docker.container.list('--filter label=generated_name')
+	if #containers ~= 1 then
+		self:fail('expecting 1 container with "generated_name" label')
+	end
+	self:cmd('rm -f '  .. containers[1].name)
+
+	self:log("start containers in both scopes")
+
+	-- run 3 containers in scope #1
+	project.id = scope1
+	self:cmd('run -ti -d alpine:3.5 ash')
+	self:cmd('run -ti -d alpine:3.5 ash')
+	self:cmd('run -ti -d alpine:3.5 ash')
+	-- run 1 container in scope #2
+	project.id = scope2
+	self:cmd('run -ti -d alpine:3.5 ash')
+
+	-- test number of containers in both scopes
+	project.id = scope2
+	containers = docker.container.list()
+	if #containers ~= 1 then
+		self:fail('expecting 1 container in scope #2')
+	end
+	project.id = scope1
+	containers = docker.container.list()
+	if #containers ~= 3 then
+		self:fail('expecting 3 containers in scope #1')
+	end
+
+	-- TODO: insert more tests here
+
+	self:log("stop + rm containers in scope #1")
+
+	-- stop and rm all containers in scope #1
+	project.id = scope1
+	containers = docker.container.list()
+	for i,container in ipairs(containers) do
+		self:cmd('stop -t 0 ' .. container.id)
+	end
+	for i,container in ipairs(containers) do
+		self:cmd('rm ' .. container.id)
+	end
+
+	self:log("rm -f containers in scope #2")
+
+	-- rm -f all containers in scope #2
+	project.id = scope2
+	containers = docker.container.list()
+	for i,container in ipairs(containers) do
+		self:cmd('rm -f ' .. container.id)
+	end
+
+	self:success()
+end
+
+-- Volume --
+
+-- check that volume create works 
+-- and prints the correct output
+function Tests:testVolumeCreate()
+	self:start('volume create output')
+	project.id = 'com.docker.test.scope.id.1'
+	local name = 'foo'
+	local out = self:cmd('volume create ' .. name)
+	if out ~= name then
+		self:fail('volume create did not print the volume\'s id')
+	end
+	self:cmd('volume rm ' .. name)
+	self:success()
+end
+
+-- Network --
+
+function Tests:testNetworkCreate()
+	self:start('network create output')
+	project.id = 'com.docker.test.scope.id.1'
+	local name = 'foo'
+	self:cmd('network create ' .. name)
+	self:cmd('network rm ' .. name)
+	self:success()
+end
+
+local tests = Tests
+
+return tests
